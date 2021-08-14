d73e6b05dcaffad542eee032e58fe9b0cf449f0e
ankitvaria <ankit.1varia@angelbroking.com>
2021-08-15 00:57:41 +0530

    - Day Four

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/pages/home_page.dart';
 import 'package:flutter_demo/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,8 +13,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.grey),
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
       routes: {
         "/": (context) => LoginPage(),
         "/login": (context) => LoginPage()
